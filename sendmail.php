eba48fd7b052387c7666c039abdb337ff53ed1ac
NulLundVoid <109977863+NulLundVoidd@users.noreply.github.com>
2025-11-04 19:26:29 -0800

    Remove sendmail.php file

--- sendmail.php
+++ sendmail.php
@@ -1,17 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = htmlspecialchars($_POST['email']);
-    $question = htmlspecialchars($_POST['question']);
 
-    $to = "tlundean@gmail.com"; // where the contact form sends to
-    $subject = "Pig Dice Support Request";
-    $message = "Email from: $email\n\nQuestion:\n$question";
-    $headers = "From: noreply@piggame.com";
-
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Thank you! Your message has been sent.";
-    } else {
-        echo "Message failed to send.";
-    }
-}
-?>
